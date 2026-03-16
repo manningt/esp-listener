2ce14840d7a585bd84e33196019217bd3a23a8ec
Tom Manning <tom@manningetal.com>
2026-03-16 10:25:23 -0400

    add twilio sms

--- listener_app.py
+++ listener_app.py
@@ -3,21 +3,28 @@ from support import write_rtc_memory, restore_from_rtc_memory, \
 from machine import Pin, I2S
 import math
 import sys
+import requests
 
-def calculate_rms(data, offset):
-    sum_squares = 0.0
-    num_samples = len(data) // 2
-    for i in range(offset, len(data), 2):
-        sample = int.from_bytes(data[i:i+2], 'little')
-        # Signed 16-bit
-        if sample >= 32768:
+# MIC_BUFFER_SIZE = (8000 * 2 * 3)
+MIC_BUFFER_SIZE = (8000 * 2)
+
+def get_rms(audio_in):
+   mic_samples = bytearray(MIC_BUFFER_SIZE)
+   num_read = audio_in.readinto(mic_samples)
+   if num_read != MIC_BUFFER_SIZE:
+      print(f"Error: samples read={num_read}; expected={MIC_BUFFER_SIZE}")
+      rms = 0
+   else:
+      sum_squares = 0.0
+      num_samples = MIC_BUFFER_SIZE // 2
+      for i in range(0, MIC_BUFFER_SIZE, 2):
+         sample = int.from_bytes(mic_samples[i:i+2], 'little')
+         # Signed 16-bit
+         if sample >= 32768:
             sample -= 65536
-        sum_squares += sample * float(sample)
-            
-    # print(f'offset: {offset}, len(data): {len(data)}, sum_sq: {sum_squares}')
-    if num_samples == 0:
-        return 0
-    return math.sqrt(sum_squares / num_samples)
+         sum_squares += sample * float(sample)
+      rms = math.sqrt(sum_squares / num_samples)
+   return rms
 
 def main():
    try:
@@ -25,9 +32,6 @@ def main():
       tinypico.set_dotstar_power(False)
    except:
       pass
-   I2S_PORT_ID = 1
-   # MIC_BUFFER_SIZE = (8000 * 2 * 3)
-   MIC_BUFFER_SIZE = (8000 * 2)
 
    count_state = restore_from_rtc_memory()
    if count_state is None:
@@ -55,21 +59,16 @@ def main():
       print("Quitting: Missing 'sampling' in config json file")
       sys.exit(0)
 
-
    if 'i2s_pins' in config:
+      I2S_PORT_ID = 1
       audio_in = I2S(I2S_PORT_ID, mode=I2S.RX, \
          sck=config['i2s_pins']['sck'], sd=config['i2s_pins']['sd'], ws=config['i2s_pins']['wstrobe'], \
          bits=16, format=I2S.MONO, rate=8000, ibuf=MIC_BUFFER_SIZE)
-      mic_samples = bytearray(MIC_BUFFER_SIZE)
       select_pin = Pin(config['i2s_pins']['select'], Pin.OUT)
-      select_pin.value(0)    
-      num_read = audio_in.readinto(mic_samples)
-      if num_read != MIC_BUFFER_SIZE:
-         print(f"Error: samples read={num_read}; expected={MIC_BUFFER_SIZE}")
-      else:
-         rms = calculate_rms(mic_samples, 0)
-         print(f'RMS: {rms:.2f}')
-
+      select_pin.value(0)
+      rms = get_rms(audio_in)
+      print(f'RMS: {rms:.2f}')
+      if rms > 0:
          increment_thold_count = False
          if config['sampling']['below_above'] == 1:
             if rms > config['sampling']['threshold']:
@@ -105,7 +104,21 @@ def main():
       else:
          print("No wifi config")
 
-      if "ftp" in config:
+      mem_status() #free up memory (the audio buffer) for urequest
+
+      if station.isconnected() and "twilio" in config:
+         url = config['twilio']['api'].replace('_sid_',config['twilio']['sid'])
+         msg = "test-1"
+         response = requests.post(
+            url, 
+            data=f"To={config['twilio']['to']}&From={config['twilio']['from']}&Body={msg}",
+            auth=(config['twilio']['sid'], config['twilio']['token']),
+            headers={'Content-Type': 'application/x-www-form-urlencoded'}
+         )
+         print(f"sms response: code={response.status_code}; text={response.text}")
+         response.close()
+
+      if station.isconnected() and "ftp" in config:
          host = config['ftp']['host']
          ftp = setup_ftp(host, config['ftp']['user'], config['ftp']['password'])
          if ftp is None:
